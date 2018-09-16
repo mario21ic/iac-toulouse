038a539c10bb568eef57d359b0a0f9b006cba918
Mario Inga <mario@yarkan.pe>
2018-09-15 20:02:11 -0500

    agregando elb

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_elb" "my_elb" {
     healthy_threshold   = 10
     unhealthy_threshold = 3
     timeout             = 15
-    target              = "/"
+    target              = ""
     interval            = 30
   }
 
